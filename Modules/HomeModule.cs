9874c634861fba403dbe0cb43c09ac718d21efe0
StephanK9 <stephane.khoury@gmail.com>
2016-11-18 12:04:45 -0800

    fixed the naming conventions for the Get/Post to match the correct ones for the exercise (/contact, /contact/new

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,9 +9,12 @@ namespace AddressBook
     public HomeModule()
     {
       Get["/"] = _ => {
+        return View["homepage.cshtml"];
+      };
+      Get["/contact"] = _ => {
         return View["add_new_contact.cshtml"];
       };
-      Post["/contact_added"] = _ => {
+      Post["/contact/new"] = _ => {
         string inputName = Request.Form["new-name"];
         int inputPhone = int.Parse(Request.Form["new-phone"]);
         string inputAddress = Request.Form["new-address"];
@@ -25,7 +28,7 @@ namespace AddressBook
       };
       Post["/clear"] = _ => {
         Contact.ClearAllContacts();
-        return View["clear.cshtml"];
+        return View["clear"];
       };
     }
   }
